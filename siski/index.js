b3d0c5f87f039103d1f84dd457a7b9a3e1d16a5a
AldiBoomich <aaldik2017@gmail.com>
2026-04-18 03:20:14 +0500

    Add files via upload

--- siski/index.js
+++ siski/index.js
@@ -1,35 +1,25 @@
-var SisKi = 228;
-if (SisKi % 2 == 0) {
-  console.log("иди нахуй");
+for (let i = 1; i < 20; i++) {
+  if (i % 4 == 0) {
+    continue;
+  }
+  console.log(i);
 }
-let age = 8;
-let discount = age < 18 ? "10%" : (age >= 18 && age <= 65) ? "20%" : age >= 65 ? "30%" : "сиебался" ;
-console.log(discount)
+j = +prompt("введите число");
+result = 1;
+for (let k = 1; k <= j; k++) {
+  result = result * k;
+}
+console.log(result);
 
-let username = prompt("name");
-let password = prompt("password");
-switch (true){
-    case (username === "terpila" && password === "228nikogoneprosim"):
-        console.log("все норм");
-        break;
-        default:
-            console.log("гандурас")
+let board = "";
+for (let l = 1; l <= 8; l++) {
+  for (let m = 1; m <= 8; m++) {
+    if ((l + m) % 2 === 0) {
+      board += "*";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
 }
-let price;
-let ves = +prompt("укажите вес товара  в кг");
-let kef = prompt("выберете пакет услуг (bomj) (sexpress) (lux) ");
-if(ves < 2){
-    price = 1000
-}else if(ves >= 2 && ves <= 5){
-    price = 5000
-}else if(ves > 5){
-    price = 15000
-}
-if(kef == "bomj"){
-    price *= 1
-}else if(kef == "sexpress"){
-    price *= 1.5
-}else if(kef == "lux"){
-    price *= 2
-}
-console.log(`${price} tg`);
+console.log(board);
